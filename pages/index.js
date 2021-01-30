49daae929292d8865ecc350577014c77ed20cf44
Victoria Duraes <victoria.oliveira@totvs.com.br>
2021-01-30 18:15:02 -0300

    chore: create QuizContainer component

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import styled from 'styled-components';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizzBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
