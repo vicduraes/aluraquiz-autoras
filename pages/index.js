92f11b30a58aad5480e1cb217cba5fa99260b6f9
Victoria Duraes <victoria.oliveira@totvs.com.br>
2021-01-30 21:45:12 -0300

    fix: add correct link in github corner

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://www.github.com/vicduraes" />
+      <GitHubCorner projectUrl="https://github.com/vicduraes/aluraquiz-autoras" />
     </QuizBackground>
   );
 }
